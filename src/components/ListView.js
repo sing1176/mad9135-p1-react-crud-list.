36e7c89a08344f174e1a1a4fa8b923714a2a90b4
Sandeep Saini <sing1176@algonquinlive.com>
2021-11-03 14:23:43 -0400

    Added edit fuction base

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -14,31 +14,55 @@ const ListView = ({ data, setData }) => {
 		setData([...data]);
 	};
 
+	let editedObj = {
+		id: Math.random().toString(36).substr(2, 9),
+		name: 'Catwoman',
+		creator: 'DC Comics',
+		url: 'https://www.dccomics.com/',
+	};
+
+	const editItem = (e) => {
+		e.preventDefault();
+		console.log('editing item');
+		let id = e.target.name;
+		const index = data.findIndex((item) => item.id === id);
+		console.log(id);
+		if (index > -1) {
+			
+			data.splice(index, 1, editedObj );
+		}
+		setData([...data]);
+	};
+
 	console.log(data);
     
 	return (
 		<>
 			<Container>
-				{ 
-
-				data.length <= 0 ? <h1>Please add Data</h1> :
-				data.map((item) => (
-					<div key={item.id}>
-						<div className="infoCard">
-							<div className="info">
-								<h2>{item.name}</h2>
-								<p>{item.creator}</p>
-								<p>{item.url}</p>
-							</div>
-							<div className="buttons">
-								<Button variant="warning"> Edit </Button>
-								<Button variant="danger" name={item.id} onClick={deleteItem}>
-									Delete
-								</Button>
+				{data.length <= 0 ? (
+					<h1>Please add Data</h1>
+				) : (
+					data.map((item) => (
+						<div key={item.id}>
+							<div className="infoCard">
+								<div className="info">
+									<h2>{item.name}</h2>
+									<p>{item.creator}</p>
+									<p>{item.url}</p>
+								</div>
+								<div className="buttons">
+									<Button variant="warning" name={item.id} onClick={editItem}>
+										{' '}
+										Edit{' '}
+									</Button>
+									<Button variant="danger" name={item.id} onClick={deleteItem}>
+										Delete
+									</Button>
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					))
+				)}
 			</Container>
 		</>
 	);
