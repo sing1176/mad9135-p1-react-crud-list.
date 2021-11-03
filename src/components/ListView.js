b3db333d112e66619aaaad33a0c81fb42b03ff68
Sandeep Saini <sing1176@algonquinlive.com>
2021-11-03 17:16:21 -0400

    Added edit and replace functionality

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,7 +1,12 @@
 import { Button, Container } from 'react-bootstrap';
+import { useState } from 'react';
 
 const ListView = ({ data, setData }) => {
-
+	const [editing, setEditing] = useState(false);
+	const [editHeroName, setEditHeroName] = useState('');
+	const [editCreator, setEditCreator] = useState('');
+	const [editLink, setEditLink] = useState('');
+	const [id , setId ]= useState('');
 	const deleteItem = (e) => {
 		e.preventDefault();
 		let id = e.target.name;
@@ -9,36 +14,63 @@ const ListView = ({ data, setData }) => {
 		if (index > -1) {
 			data.splice(index, 1);
 		}
-
-		console.log(data);
 		setData([...data]);
 	};
 
 	let editedObj = {
 		id: Math.random().toString(36).substr(2, 9),
-		name: 'Catwoman',
-		creator: 'DC Comics',
-		url: 'https://www.dccomics.com/',
+		name: editHeroName,
+		creator: editCreator,
+		url: editLink,
 	};
 
-	const editItem = (e) => {
+	
+	function startEdit(e) {
 		e.preventDefault();
-		console.log('editing item');
+		setEditing(true);
 		let id = e.target.name;
-		const index = data.findIndex((item) => item.id === id);
+		setId(id)
+	};
+
+	function saveEdit(){
 		console.log(id);
+		const index = data.findIndex((item) => item.id === id);
 		if (index > -1) {
-			
-			data.splice(index, 1, editedObj );
+			data.splice(index, 1, editedObj);
 		}
 		setData([...data]);
-	};
+	}
 
-	console.log(data);
-    
 	return (
 		<>
 			<Container>
+				{editing ? (
+					<form className="form" onSubmit={saveEdit}>
+						<input
+							value={editHeroName}
+							onChange={(e) => setEditHeroName(e.target.value)}
+							placeholder="Superhero Name"
+							type="text"
+							required
+						/>
+						<input
+							value={editCreator}
+							onChange={(e) => setEditCreator(e.target.value)}
+							placeholder="Creator"
+							type="text"
+							required
+						/>
+						<input
+							value={editLink}
+							onChange={(e) => setEditLink(e.target.value)}
+							placeholder="Add link"
+							type="text"
+							required
+						/>
+						<button type="submit">Save</button>
+					</form>
+				) : null}
+
 				{data.length <= 0 ? (
 					<h1>Please add Data</h1>
 				) : (
@@ -51,9 +83,12 @@ const ListView = ({ data, setData }) => {
 									<p>{item.url}</p>
 								</div>
 								<div className="buttons">
-									<Button variant="warning" name={item.id} onClick={editItem}>
-										{' '}
-										Edit{' '}
+									<Button
+										variant="warning"
+										name={item.id}
+										onClick={startEdit}
+									>
+										Edit
 									</Button>
 									<Button variant="danger" name={item.id} onClick={deleteItem}>
 										Delete
